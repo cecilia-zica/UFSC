8207fb8334f050765f742392353b5a81c9510341
Cecilia Zica Camargo <ceciliazica8789@gmail.com>
2025-09-09 21:45:48 -0300

    Adicionando práticas em Web

---
+++ ProgramacaoParaWeb/praticaJS1/js/exercicio1.js
@@ -0,0 +1,15 @@
+//atualize esta função
+function tratadorDeCliqueExercicio1() {
+
+    let valorComoString = window.prompt("Insira um valor numérico: ")
+
+    console.log("O valor informado é: "+valorComoString)
+    console.log("O valor informado é do tipo: "+ typeof valorComoString)
+
+    let valorComoInteiro = parseInt(let valorComoString()) //modificado aqui
+
+    console.log("O valor como inteiro é: "+valorComoInteiro)
+    console.log("O valor como inteiro é do tipo: "+typeof valorComoInteiro)
+}
+
+tratadorDeCliqueExercicio1()
